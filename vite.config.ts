c3ec9e6b090d894a77b6a4b02ad06d69efc33b44
Luis Mendez <luis.mendez@generic.de>
2025-08-03 14:32:38 +0200

    add vite static copy plugin, streamlined tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,55 +1,62 @@
-import { UserConfig, defineConfig } from "vite";
+import { defineConfig } from "vite";
 import path from "path";
 import builtins from "builtin-modules";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
-export default defineConfig(async ({ mode }) => {
-	const { resolve } = path;
-	const prod = mode === "production";
+export default defineConfig(({ mode }) => {
+  const prod = mode === "production";
+  const outDir = "dist/.obsidian/plugins/convert-to-docx";
 
-	return {
-		plugins: [],
-		resolve: {
-			alias: {
-				"@": path.resolve(__dirname, "./src"),
-			},
-		},
-		build: {
-			lib: {
-				entry: resolve(__dirname, "src/main.ts"),
-				name: "main",
-				fileName: () => "main.js",
-				formats: ["cjs"],
-			},
-			minify: prod,
-			sourcemap: prod ? false : "inline",
-			cssCodeSplit: false,
-			emptyOutDir: false,
-			outDir: "dist/.obsidian/plugins/convert-to-docx",
-			rollupOptions: {
-				input: {
-					main: resolve(__dirname, "src/main.ts"),
-				},
-				output: {
-					entryFileNames: "main.js",
-					assetFileNames: "styles.css",
-				},
-				external: [
-					"obsidian",
-					"electron",
-					"@codemirror/autocomplete",
-					"@codemirror/collab",
-					"@codemirror/commands",
-					"@codemirror/language",
-					"@codemirror/lint",
-					"@codemirror/search",
-					"@codemirror/state",
-					"@codemirror/view",
-					"@lezer/common",
-					"@lezer/highlight",
-					"@lezer/lr",
-					...builtins,
-				],
-			},
-		},
-	} as UserConfig;
+  return {
+    resolve: {
+      alias: { "@": path.resolve(__dirname, "src") },
+    },
+    build: {
+      target: "esnext",
+      minify: prod,
+      sourcemap: prod ? false : "inline",
+      cssCodeSplit: false,
+      emptyOutDir: false,
+      outDir,
+      lib: {
+        entry: path.resolve(__dirname, "src/main.ts"),
+        formats: ["cjs"],
+        fileName: () => "main.js",
+      },
+      rollupOptions: {
+        external: [
+          "obsidian",
+          "electron",
+          "@codemirror/autocomplete",
+          "@codemirror/collab",
+          "@codemirror/commands",
+          "@codemirror/language",
+          "@codemirror/lint",
+          "@codemirror/search",
+          "@codemirror/state",
+          "@codemirror/view",
+          "@lezer/common",
+          "@lezer/highlight",
+          "@lezer/lr",
+          ...builtins,
+        ],
+        output: {
+          entryFileNames: "main.js",
+          assetFileNames: "styles.css",
+        },
+      },
+      commonjsOptions: {
+        include: [/node_modules/],
+      },
+    },
+    plugins: [
+      viteStaticCopy({
+        targets: [
+          { src: "manifest.json", dest: '.' },
+          { src: "README.md", dest: '.' },
+          { src: "LICENSE", dest: '.' },
+        ],
+      }),
+    ],
+  };
 });
